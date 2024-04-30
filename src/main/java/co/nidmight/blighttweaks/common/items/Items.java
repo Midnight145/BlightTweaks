955757455810401e4294b1c84c17ebbc8f05becd
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-04-30 01:34:37 -0500

    i forgor resources

--- src/main/java/co/nidmight/blighttweaks/common/items/Items.java
+++ src/main/java/co/nidmight/blighttweaks/common/items/Items.java
@@ -3,6 +3,7 @@ package co.nidmight.blighttweaks.common.items;
 import net.minecraft.item.Item;
 
 public class Items {
+
     public static Item boundRing;
     public static Item alienTome;
     public static Item worldOreKiller;
