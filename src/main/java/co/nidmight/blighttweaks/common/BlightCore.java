d0a19b21d251ab39b9a06fd226210dc205cebf6a
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-04-30 01:34:05 -0500

    remap moved blightbuster items if necessary

--- src/main/java/co/nidmight/blighttweaks/common/BlightCore.java
+++ src/main/java/co/nidmight/blighttweaks/common/BlightCore.java
@@ -1,5 +1,8 @@
 package co.nidmight.blighttweaks.common;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import co.nidmight.blighttweaks.common.handlers.ChunkHandler;
 import co.nidmight.blighttweaks.common.handlers.FoodHandler;
 import co.nidmight.blighttweaks.common.handlers.InteractHandler;
@@ -14,10 +17,12 @@ import noppes.npcs.blocks.BlockBlood;
 @Mod(modid = BTStrings.MOD_ID, name = BTStrings.MOD_NAME, version = BTStrings.VERSION)
 public class BlightCore {
 
+    public static Logger logger = LogManager.getLogger(BTStrings.MOD_ID);
+
     @Mod.EventHandler()
     public void preInit(FMLInitializationEvent event) {
         Items.init();
-    }   
+    }
 
     @Mod.EventHandler()
     public void init(FMLInitializationEvent event) {
@@ -34,23 +39,28 @@ public class BlightCore {
     }
 
     @Mod.EventHandler
-    public void missingMappings(FMLMissingMappingsEvent event) {
+    public void missingMappings(FMLMissingMappingsEvent event) throws Exception {
         for (FMLMissingMappingsEvent.MissingMapping mapping : event.getAll()) {
+            logger.info("Found missing mapping: {}", mapping.name);
             if (!mapping.name.startsWith("blightbuster")) {
                 continue;
             }
             switch (mapping.name) {
                 case "blightbuster:boundRing":
+                    logger.info("Remapping blightbuster:boundRing to " + Items.boundRing.getUnlocalizedName());
                     mapping.remap(Items.boundRing);
                     break;
-                case "blightbuster:researchNote":
+                case "blightbuster:researchnote":
+                    logger.info("Remapping blightbuster:researchnote to " + Items.alienTome.getUnlocalizedName());
                     mapping.remap(Items.alienTome);
                     break;
                 case "blightbuster:worldOreKiller":
+                    logger
+                        .info("Remapping blightbuster:worldOreKiller to " + Items.worldOreKiller.getUnlocalizedName());
                     mapping.remap(Items.worldOreKiller);
                     break;
                 default:
-                    break;
+                    logger.error("Unknown mapping: {}", mapping.name);
             }
         }
     }
