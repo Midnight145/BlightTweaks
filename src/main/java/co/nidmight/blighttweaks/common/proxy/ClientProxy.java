1c3e0185a8957320deff9968d1b8c4915f1f590b
Midnight145 <23142346+Midnight145@users.noreply.github.com>
2025-11-10 19:33:36 -0600

    call super.init() in clientproxy (#4)

--- src/main/java/co/nidmight/blighttweaks/common/proxy/ClientProxy.java
+++ src/main/java/co/nidmight/blighttweaks/common/proxy/ClientProxy.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.FMLCommonHandler;
 public class ClientProxy extends CommonProxy {
 
     public void init() {
+        super.init();
         FMLCommonHandler.instance()
             .bus()
             .register(new KeybindHandler());
